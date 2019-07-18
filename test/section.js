d5bc69b5e3563cbd409a9cc0d6b574ff640e5ee7
bono lv <lvscar@gmail.com>
2019-07-19 04:56:34 +0800

    Finding result spanning multiple document nodes supported in createTreeWalker available environment.

--- test/section.js
+++ test/section.js
@@ -28,6 +28,45 @@ describe("section", function() {
                                 assert.equal(results[1].excerpt, "...ut it was\n\t\tall dark overhead; before her was another long passage and the White Rabbit was still\n\t\tin sight, hurrying down it. There was not a moment...");
                        });
                 });
+        });
 
+        it("finds result that spanning multiple document nodes, tag at beginning", function() {
+                var book = ePub("./fixtures/alice/", {width: 400, height: 400});
+                return book.ready.then(function() {
+                        // <p>"Oh, I <i>beg</i> your pardon!" she exclaimed in a tone of great dismay. </p>
+                        var section = book.section("chapter_010.xhtml");
+                        return section.load().then(function() {
+                                var results = section.find("beg your pardon");
+                                assert.equal(results.length, 1);
+                                assert.equal(results[0].cfi, "epubcfi(/6/26[chapter_010]!/4/2/6,/2/1:0,/3:12)");
+                                assert.equal(results[0].excerpt,'beg your pardon!" she exclaimed in a tone of great dismay.');
+                        });
+                });
+        });
+
+        it("finds result that spanning multiple document nodes, tag at ending", function() {
+                var book = ePub("./fixtures/alice/", {width: 400, height: 400});
+                return book.ready.then(function() {
+                        var section = book.section("chapter_010.xhtml");
+                        return section.load().then(function() {
+                                var results = section.find("I beg");
+                                assert.equal(results.length, 1);
+                                assert.equal(results[0].cfi, "epubcfi(/6/26[chapter_010]!/4/2/6,/1:5,/2/1:3)");
+                                assert.equal(results[0].excerpt,'"Oh, I beg');
+                        });
+                });
+        });
+
+        it("finds result that spanning multiple document nodes, tag at middle", function() {
+                var book = ePub("./fixtures/alice/", {width: 400, height: 400});
+                return book.ready.then(function() {
+                        var section = book.section("chapter_010.xhtml");
+                        return section.load().then(function() {
+                                var results = section.find("I beg your pardon");
+                                assert.equal(results.length, 1);
+                                assert.equal(results[0].cfi, "epubcfi(/6/26[chapter_010]!/4/2/6,/1:5,/3:12)");
+                                assert.equal(results[0].excerpt,'"Oh, I beg your pardon!" she exclaimed in a tone of great dismay.');
+                        });
+                });
         });
 });
